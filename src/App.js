dfc6a1eaa84b9842a679708dc89aee31a1168279
debajit13 <mallickdebajit3@gmail.com>
2021-03-26 00:10:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./assets/logo.png";
+import sorry from "./assets/sorry.png";
+import background from "./assets/background.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="container-fluid">
+        <div class="row justify-content-center">
+          <div class="col-2">
+            <img src={logo} alt="logo" class="logo ml-5" />
+          </div>
+        </div>
+        <div class="row ml-5">
+          <div class="col-md-6">
+            <img src={sorry} alt="sorry" class="sorry-img" />
+          </div>
+          <div class="col-md-6">
+            <img src={background} alt="background" class="background" />
+            <p class="message">
+              Sorry{" "}
+              <span class="text">
+                for the inconvenience caused! <br />
+                We are currently in our
+              </span>{" "}
+              rebranding
+              <span class="text"> phase.</span>
+            </p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col">
+            <p class="connect">
+              Feel free to connect with us at
+              <span class="email"> info@blackspektro.com</span>
+            </p>
+          </div>
+        </div>
+
+        <div class="row mt-5 justify-content-center">
+          <div class="col-3 mt-5">
+            <p class="credit">
+              Illustration credit:
+              <a href="https://storyset.com/people" target="_blank">
+                Illustration by Freepik Storyset
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
